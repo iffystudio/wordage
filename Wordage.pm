788cb7d05868190524b6047d57f72af8468b934d
Bryn Dyment <bryndyment@gmail.com>
2020-07-23 23:30:02 +0000

    initial commit

---
+++ Wordage.pm
@@ -0,0 +1,18 @@
+package Wordage;
+
+use strict;
+use utf8;
+
+use constant STYLE_1 => 1;
+
+use vars qw (
+  $ALTS
+  );
+
+BEGIN {
+
+  $ALTS = 'A-Z0-9';
+
+  };
+
+1;
---
+++ wordage.pl
@@ -0,0 +1,7 @@
+use strict;
+
+use Hoo;
+
+use Wordage;
+
+1;
